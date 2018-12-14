6a1effb6fe1617c150fc66aa6f846dc3f2b5f586
Michal Ziulek <michalziulek@gmail.com>
2018-12-14 15:53:33 +0100

    Add files via upload

---
+++ src/Demo.cpp
@@ -0,0 +1,59 @@
+#include "Pch.h"
+#include "Demo.h"
+
+
+void Demo::setup()
+{
+}
+
+void Demo::update()
+{
+}
+
+void Demo::draw()
+{
+}
+
+void Demo::keyPressed(int key)
+{
+}
+
+void Demo::keyReleased(int key)
+{
+}
+
+void Demo::mouseMoved(int x, int y)
+{
+}
+
+void Demo::mouseDragged(int x, int y, int button)
+{
+}
+
+void Demo::mousePressed(int x, int y, int button)
+{
+}
+
+void Demo::mouseReleased(int x, int y, int button)
+{
+}
+
+void Demo::mouseEntered(int x, int y)
+{
+}
+
+void Demo::mouseExited(int x, int y)
+{
+}
+
+void Demo::windowResized(int w, int h)
+{
+}
+
+void Demo::gotMessage(ofMessage msg)
+{
+}
+
+void Demo::dragEvent(ofDragInfo dragInfo)
+{ 
+}
---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include "Pch.h"
+#include "Demo.h"
+
+
+int main()
+{
+	ofSetupOpenGL(1024, 768, OF_WINDOW);
+	ofRunApp(new Demo());
+}
